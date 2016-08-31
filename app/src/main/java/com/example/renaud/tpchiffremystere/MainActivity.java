5a8589fc1c8174c36a40f14aee1826b4a95f783c
bigwanjeog <wenaud@live.fr>
2016-08-31 09:07:31 +0200

    Ajout de la fonction loadActivity pour rejouer

--- app/src/main/java/com/example/renaud/tpchiffremystere/MainActivity.java
+++ app/src/main/java/com/example/renaud/tpchiffremystere/MainActivity.java
@@ -15,11 +15,12 @@ public class MainActivity extends AppCompatActivity {
 
     Random rdm = new Random();
     int numTest;//numéro entré a tester
-    int numMystere = rdm.nextInt(100) + 1;
-    int cptTest = 0;//nombre de test effectué
+    int numMystere;
+    int cptTest;//nombre de test effectué
     final static int NBDETEST =4;//nombre de test a faire
 
     Button activityMainBtnValider;
+    Button activityMainBtnReset;
     EditText activityMainTfNumTest;
     TextView activityMainTvPlusMoins;
     TextView activityMainTvEssai;
@@ -27,9 +28,18 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        loadActivity();
+
+    }
+
+    public void loadActivity () {
         setContentView(R.layout.activity_main);
 
+        cptTest = 0;
+        numMystere = rdm.nextInt(100) + 1;
+
         activityMainBtnValider = (Button) findViewById(R.id.activityMainBtnValider);
+        activityMainBtnReset = (Button) findViewById(R.id.activityMainBtnReset);
         activityMainTfNumTest = (EditText) findViewById(R.id.activityMainTfNumTest);
         activityMainTvPlusMoins = (TextView) findViewById(R.id.activityMainTvPlusMoins);
         activityMainTvEssai = (TextView) findViewById(R.id.activityMainTvEssai);
@@ -68,13 +78,25 @@ public class MainActivity extends AppCompatActivity {
                 activityMainTvPlusMoins.setTextColor(Color.GREEN);
                 activityMainTvPlusMoins.setText("GG");
                 activityMainBtnValider.setEnabled(false);
+                reset();
             }
         } else{
             //TODO recommencer
             activityMainTvPlusMoins.setText("PERDU !!\n La bonne réponse était " + numMystere);
             activityMainBtnValider.setEnabled(false);
+            reset();
         }
         cptTest++;
         activityMainTvEssai.setText((NBDETEST + 1 - cptTest) + " essais restants");
+    }
+
+    public void reset() {
+        activityMainBtnReset.setVisibility(View.VISIBLE);
+        activityMainBtnReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadActivity();
+            }
+        });
     }
 }
